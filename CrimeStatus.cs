d9e7dbe40363f0e39065d5c397464f3946d536b1
韩镇先 <hanzhenxian@hanzhenxiandeMacBook-Pro.local>
2019-04-05 17:35:06 -0500

    complete

---
+++ CrimeStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace crime
+{
+    public class CrimeStats
+    {
+
+        public int Year { get; set; }
+        public int Population { get; set; }
+        public int Murders { get; set; }
+        public int Rapes { get; set; }
+        public int Robberies { get; set; }
+        public int ViolentCrimes { get; set; }
+        public int Thefts { get; set; }
+        public int MotorVehicleThefts { get; set; }
+    }
+}
